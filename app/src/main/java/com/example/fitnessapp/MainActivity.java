a72953421854861b4ecab2f58bc2d2a1183f5749
Kevin Zong <7kevinzong@gmail.com>
2020-01-13 21:56:16 -0500

    Added navigation bar functionality

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fitnessapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
     FrameLayout frameLayout;
 
+    private HomeFragment homeFragment;
+    private ExercisesFragment exercisesFragment;
+    private CalendarFragment calendarFragment;
+    private SettingsFragment settingsFragment;
+    private ProgressFragment progressFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +29,48 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNav = findViewById(R.id.bottomNavigationView);
         frameLayout = findViewById(R.id.frameLayout);
+
+        homeFragment = new HomeFragment();
+        exercisesFragment = new ExercisesFragment();
+        calendarFragment = new CalendarFragment();
+        settingsFragment = new SettingsFragment();
+        progressFragment = new ProgressFragment();
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+
+                    case R.id.navigation_calendar:
+                        InitializeFragment(calendarFragment);
+                        return true;
+
+                    case R.id.navigation_exercises:
+                        InitializeFragment(exercisesFragment);
+                        return true;
+
+                    case R.id.navigation_home:
+                        InitializeFragment(homeFragment);
+                        return true;
+
+                    case R.id.navigation_progress:
+                        InitializeFragment(progressFragment);
+                        return true;
+
+                    case R.id.navigation_settings:
+                        InitializeFragment(settingsFragment);
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void InitializeFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/fitnessapp/ProgressFragment.java
+++ app/src/main/java/com/example/fitnessapp/ProgressFragment.java
@@ -13,10 +13,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExercisesFragment extends Fragment {
+public class ProgressFragment extends Fragment {
 
 
-    public ExercisesFragment() {
+    public ProgressFragment() {
         // Required empty public constructor
     }
 
@@ -25,7 +25,7 @@ public class ExercisesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercises, container, false);
+        return inflater.inflate(R.layout.fragment_progress, container, false);
     }
 
 }
